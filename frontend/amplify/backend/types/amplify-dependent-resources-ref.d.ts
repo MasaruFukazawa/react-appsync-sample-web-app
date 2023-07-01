1d678558870cfba65511ae9e9df22a0c3a890604
Masaru Fukazawa <upw.ponsuke@gmail.com>
2023-07-02 02:21:04 +0900

    amplifyインスコ

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactappsyncsamplewe5fe228b9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
